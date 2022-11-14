a217bd456f9c31ea599363a2ecb10a3bce271a59
Tossawat <51531950+tanza4432@users.noreply.github.com>
2022-11-14 22:32:31 +0700

    Update TabOneStation.dart

--- Project/lib/Application/Station/TabOneStation.dart
+++ Project/lib/Application/Station/TabOneStation.dart
@@ -58,156 +58,170 @@ class TabOneStation extends StatelessWidget {
     station.sort((a, b) => b.CURR_STATUS.compareTo(a.CURR_STATUS));
     return ListView.builder(
       itemCount: station.length,
-      itemBuilder: (context, int i) => Column(
-        children: [
-          Ink(
-              child: new ListTile(
-            leading: Container(
-              child: AvatarGlow(
-                glowColor: Colors.blue,
-                endRadius: 40.0,
-                duration: Duration(milliseconds: 2000),
-                repeat: true,
-                showTwoGlows: true,
-                repeatPauseDuration: Duration(milliseconds: 200),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 18.0,
-                      child: CircleAvatar(
-                        radius: 0,
-                        backgroundColor: Colors.greenAccent,
+      itemBuilder: (context, int i) {
+        print(station[i].CURR_STATUS_WL);
+        return Column(
+          children: [
+            Ink(
+                child: new ListTile(
+              leading: Container(
+                child: AvatarGlow(
+                  glowColor: Colors.blue,
+                  endRadius: 40.0,
+                  duration: Duration(milliseconds: 2000),
+                  repeat: true,
+                  showTwoGlows: true,
+                  repeatPauseDuration: Duration(milliseconds: 200),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 18.0,
+                        child: CircleAvatar(
+                          radius: 0,
+                          backgroundColor: Colors.greenAccent,
+                        ),
+                        backgroundColor: station[i].CURR_STATUS == "0"
+                            ? Colors.green
+                            : station[i].CURR_STATUS == "1"
+                                ? Colors.yellow
+                                : station[i].CURR_STATUS == "2"
+                                    ? Colors.red
+                                    : station[i].CURR_STATUS == "3"
+                                        ? Colors.white
+                                        : station[i].CURR_STATUS == "4"
+                                            ? Colors.grey
+                                            : station[i].CURR_STATUS == "5"
+                                                ? Colors.black.withOpacity(0.8)
+                                                : station[i].CURR_STATUS == "6"
+                                                    ? Colors.yellow
+                                                    : station[i].CURR_STATUS ==
+                                                            "7"
+                                                        ? Colors.red
+                                                        : Colors.black
+                                                            .withOpacity(0.8),
                       ),
-                      backgroundColor: station[i].CURR_STATUS == "0"
-                          ? Colors.green
-                          : station[i].CURR_STATUS == "1"
-                              ? Colors.yellow
-                              : station[i].CURR_STATUS == "2"
-                                  ? Colors.red
-                                  : station[i].CURR_STATUS == "3"
-                                      ? Colors.white
-                                      : station[i].CURR_STATUS == "4"
-                                          ? Colors.grey
-                                          : station[i].CURR_STATUS == "5"
-                                              ? Colors.black
-                                              : station[i].CURR_STATUS == "6"
-                                                  ? Colors.yellow
-                                                  : station[i].CURR_STATUS ==
-                                                          "7"
-                                                      ? Colors.red
-                                                      : Colors.black,
-                    ),
-                    Positioned(
-                      bottom: station[i].CURR_STATUS_WL == "6" ||
-                              station[i].CURR_STATUS_WL == "7"
-                          ? null
-                          : 0,
-                      top: station[i].CURR_STATUS_WL == "6" ||
-                              station[i].CURR_STATUS_WL == "7"
-                          ? 0
-                          : null,
-                      child: Container(
-                        child: station[i].CURR_STATUS_WL == "6" ||
+                      Positioned(
+                        bottom: station[i].CURR_STATUS_WL == "6" ||
                                 station[i].CURR_STATUS_WL == "7"
-                            ? Transform.rotate(
-                                alignment: Alignment.center,
-                                angle: 180 * math.pi / 180,
-                                child: Icon(
+                            ? null
+                            : 0,
+                        top: station[i].CURR_STATUS_WL == "6" ||
+                                station[i].CURR_STATUS_WL == "7"
+                            ? 0
+                            : null,
+                        child: Container(
+                          child: station[i].CURR_STATUS_WL == "6" ||
+                                  station[i].CURR_STATUS_WL == "7"
+                              ? Transform.rotate(
+                                  alignment: Alignment.center,
+                                  angle: 180 * math.pi / 180,
+                                  child: Icon(
+                                    Icons.change_history_rounded,
+                                    size: 40,
+                                    shadows: <Shadow>[
+                                      Shadow(
+                                          color: Colors.black, blurRadius: 15.0)
+                                    ],
+                                    color: Colors.red.withOpacity(0.8),
+                                  ),
+                                )
+                              : Icon(
                                   Icons.change_history_rounded,
                                   size: 40,
-                                  color: Colors.red.withOpacity(0.8),
+                                  shadows: <Shadow>[
+                                    Shadow(
+                                        color: Colors.black, blurRadius: 15.0)
+                                  ],
+                                  color: station[i].CURR_STATUS_WL == "0"
+                                      ? Colors.green.withOpacity(0.8)
+                                      : station[i].CURR_STATUS_WL == "1"
+                                          ? Colors.yellow.withOpacity(0.8)
+                                          : station[i].CURR_STATUS_WL == "2"
+                                              ? Colors.red.withOpacity(0.8)
+                                              : station[i].CURR_STATUS_WL == "3"
+                                                  ? Colors.white
+                                                      .withOpacity(0.8)
+                                                  : station[i].CURR_STATUS_WL ==
+                                                          "4"
+                                                      ? Colors.grey
+                                                          .withOpacity(0.8)
+                                                      : Colors.black
+                                                          .withOpacity(0.8),
                                 ),
-                              )
-                            : Icon(
-                                Icons.change_history_rounded,
-                                size: 40,
-                                color: station[i].CURR_STATUS_WL == "0"
-                                    ? Colors.green.withOpacity(0.8)
-                                    : station[i].CURR_STATUS_WL == "1"
-                                        ? Colors.yellow.withOpacity(0.8)
-                                        : station[i].CURR_STATUS_WL == "2"
-                                            ? Colors.red.withOpacity(0.8)
-                                            : station[i].CURR_STATUS_WL == "3"
-                                                ? Colors.white.withOpacity(0.8)
-                                                : station[i].CURR_STATUS_WL == 4
-                                                    ? Colors.grey
-                                                        .withOpacity(0.8)
-                                                    : Colors.black
-                                                        .withOpacity(0.8),
-                              ),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            title: new Text(
-              station[i].STN_ID,
-              style: TextStyle(
-                  color: Colors.black54,
-                  fontFamily: 'Kanit',
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w400),
-            ),
-            subtitle: new Text(
-              station[i].STN_Name,
-              style: TextStyle(
-                  color: Colors.black54,
-                  fontFamily: 'Kanit',
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w200),
-            ),
-            trailing: Wrap(
-              spacing: 12, // space between two icons
-              children: <Widget>[
-                Image.asset(
-                  station[i].CURR_STATUS == "0"
-                      ? 'assets/banner/bell/green.png'
-                      : station[i].CURR_STATUS == "1"
-                          ? "assets/banner/bell/yellow.png"
-                          : station[i].CURR_STATUS == "2"
-                              ? "assets/banner/bell/red.png"
-                              : station[i].CURR_STATUS == "3"
-                                  ? "assets/banner/bell/gray.png"
-                                  : station[i].CURR_STATUS == "4"
-                                      ? "assets/banner/bell/gray.png"
-                                      : station[i].CURR_STATUS == "5"
-                                          ? "assets/banner/bell/gray.png"
-                                          : station[i].CURR_STATUS == "6"
-                                              ? "assets/banner/bell/yellow.png"
-                                              : station[i].CURR_STATUS == "7"
-                                                  ? "assets/banner/bell/red.png"
-                                                  : "assets/banner/bell/red.png",
-                  height: 45,
-                ),
-                // icon-1
+              title: new Text(
+                station[i].STN_ID,
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: 'Kanit',
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w400),
+              ),
+              subtitle: new Text(
+                station[i].STN_Name,
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: 'Kanit',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w200),
+              ),
+              trailing: Wrap(
+                spacing: 12, // space between two icons
+                children: <Widget>[
+                  Image.asset(
+                    station[i].CURR_STATUS == "0"
+                        ? 'assets/banner/bell/green.png'
+                        : station[i].CURR_STATUS == "1"
+                            ? "assets/banner/bell/yellow.png"
+                            : station[i].CURR_STATUS == "2"
+                                ? "assets/banner/bell/red.png"
+                                : station[i].CURR_STATUS == "3"
+                                    ? "assets/banner/bell/gray.png"
+                                    : station[i].CURR_STATUS == "4"
+                                        ? "assets/banner/bell/gray.png"
+                                        : station[i].CURR_STATUS == "5"
+                                            ? "assets/banner/bell/gray.png"
+                                            : station[i].CURR_STATUS == "6"
+                                                ? "assets/banner/bell/yellow.png"
+                                                : station[i].CURR_STATUS == "7"
+                                                    ? "assets/banner/bell/red.png"
+                                                    : "assets/banner/bell/red.png",
+                    height: 45,
+                  ),
+                  // icon-1
 
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.arrow_forward_ios),
-                ), // icon-2
-              ],
-            ),
-            onTap: () {
-              // await FlutterSession().set('river', basinID.toString());
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => StationPage(
-                          stn_id: station[i].STN_ID,
-                          basinID: basinID,
-                          RF: station[i].RF,
-                          WL: station[i].WL,
-                          CCTV: station[i].CURR_CCTV,
-                          data: data)));
-            },
-          )),
-          new ListBody(
-            children: <Widget>[],
-          )
-        ],
-      ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.arrow_forward_ios),
+                  ), // icon-2
+                ],
+              ),
+              onTap: () {
+                // await FlutterSession().set('river', basinID.toString());
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StationPage(
+                            stn_id: station[i].STN_ID,
+                            basinID: basinID,
+                            RF: station[i].RF,
+                            WL: station[i].WL,
+                            CCTV: station[i].CURR_CCTV,
+                            data: data)));
+              },
+            )),
+            new ListBody(
+              children: <Widget>[],
+            )
+          ],
+        );
+      },
     );
   }
 }
